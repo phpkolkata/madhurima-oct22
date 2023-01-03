fa615cce717377cfc109c8892bdfdef298dd29cb
Md Asif Iqbal <phpkolkata@gmail.com>
2023-01-03 08:00:12 +0530

    image

--- php/26.php
+++ php/26.php
@@ -1,4 +1,12 @@
 <!-- IMPORTANT ->  method="post" enctype="multipart/form-data" -->
+
+<?php
+if(isset($_REQUEST['msg'])){
+    print"<div style='color:red'>$_REQUEST[msg]</div>";
+}
+?>
+
+
 <form action="upload.php" method="post" enctype="multipart/form-data">
     Name:<input type="text" name="nm"><br>
     Image:<input type="file" name="im"><br>
--- php/upload.php
+++ php/upload.php
@@ -6,6 +6,18 @@ print_r($_FILES);
 
 extract($_FILES['im']);
 
-copy($tmp_name, "uploads/$name");
+// $id = rand(1,9999);
+$id = time();
+$name = $id."-".$name;
+
+if($error == 0){
+    if($type == "image/png" || $type == "image/jpg" || $type == "image/jpeg" || $type == "image/gif" )
+        copy($tmp_name, "uploads/$name");
+    else
+        header("location:26.php?msg=Image format not supported!");
+}
+else{
+    header("location:26.php?msg=Image upload error!");
+}
 
 print"<img src='uploads/$name'>";
